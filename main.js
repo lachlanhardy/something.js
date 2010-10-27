c8881af9c8351c86d4fd2f654fac54d0a7b6ee46
Lachlan Hardy <lachlan@lachstock.com.au>
2010-10-27 16:53:19 +1100

    some simple re-architecture

--- main.js
+++ main.js
@@ -1,31 +1,19 @@
 (function () {
-  var paperWidth = $(window).width(),
-      paperHeight = $(window).height(),
+  var paperWidth = window.document.width,
+      paperHeight = window.document.height,
       r = Raphael(0, 0, paperWidth, paperHeight);
-      
+
   r.customAttributes.name = function(name) {
     r.text(this.attr("cx"), this.attr("cy"), name);
   };
 
   something = {
     setup: function () {
-      var base = something.random(100);
+      var base = something.utils.random(100);
       while (base--) {
           something.diagonal(base);
       }
       something.getData();
-    },
-
-    random: function (multiplier) {
-      return Math.floor(Math.random() * multiplier + 1);
-    },
-
-    randomX: function () {
-      return paperWidth - something.random(paperWidth);
-    },
-
-    randomY: function () {
-      return paperHeight - something.random(paperHeight);
     },
 
     // SELECT * FROM html WHERE url="http://brooklynbeta.org/about" and xpath="//div[@id='people']/ul"
@@ -42,18 +30,17 @@
 
     draw: function(data){
       var people = $.merge(data.query.results.ul[0].li, data.query.results.ul[1].li);
-
-      $.each(people, function(i, person){
-        something.circle(20, person);
-      });
+      for (var i=0, ii = people.length; i < ii; i++) {
+          something.circle(20, people[i]);
+      };
     },
 
     circle: function (number, person) {
       var diameter = number * 2;
 
       var circle = r.circle(
-                      diameter + something.randomX(),
-                      diameter + something.randomY(),
+                      diameter + something.utils.randomX(),
+                      diameter + something.utils.randomY(),
                       number
                     ).attr({
                       fill: "hsb(" + Math.random() + "," + Math.random() + "," + Math.random() + ")",
@@ -73,11 +60,26 @@
     diagonal: function (number) {
       r.path("M0 " + (paperHeight  - number * 30) + " L" + (paperWidth - number * 30) + " 0").attr({
         stroke: "hsb(" + Math.random() + "," + Math.random() + "," + Math.random() + ")",
-        "stroke-width": something.random(10),
+        "stroke-width": something.utils.random(10),
         opacity: Math.random()
       });
     }
 
+  };
+
+  something.utils = something.utils || {};
+  something.utils = {
+    random: function (multiplier) {
+      return Math.floor(Math.random() * multiplier + 1);
+    },
+
+    randomX: function () {
+      return paperWidth - something.utils.random(paperWidth);
+    },
+
+    randomY: function () {
+      return paperHeight - something.utils.random(paperHeight);
+    }
   };
 
   $(function () {something.setup();});
